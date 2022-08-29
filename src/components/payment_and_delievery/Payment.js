aa362eb3d13358e1c47646b92c8d6dffd689e967
Shubhanshu Singh <shubhanshusingh1796@gmail.com>
2022-08-29 23:25:42 +0530

    added payment method

--- src/components/payment_and_delievery/Payment.js
+++ src/components/payment_and_delievery/Payment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import paypal from "../../assets/payment/paypal.svg";
 import visa from "../../assets/payment/visa.png";
 import masterCard from "../../assets/payment/master-card.svg";
@@ -7,6 +7,50 @@ import discover from "../../assets/payment/discover-logo.webp";
 import ideal from "../../assets/payment/ideal.png";
 
 const Payment = () => {
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  };
+
+  useEffect(() => {
+    loadScript("https://checkout.razorpay.com/v1/checkout.js");
+  });
+  async function razorPay() {
+    const data = await fetch("http://localhost:3001/razorpay", {
+      method: "POST",
+    }).then((t) => t.json());
+
+    const options = {
+      key: process.env.RAZORPAY_KEY_ID,
+      currency: data.currency,
+      amount: data.amount,
+      name: "Pay Now",
+      description: "Wallet Transaction",
+      image: "http://localhost:3001/",
+      order_id: data.id,
+      // handler: function (response) {
+      //   alert("PAYMENT ID ::" + response.razorpay_payment_id);
+      //   alert("ORDER ID :: " + response.razorpay_order_id);
+      // },
+      prefill: {
+        name: "Shubhanshu Singh",
+        email: "shubhanshusingh@example.com",
+        contact: "9999999999",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
   return (
     <>
       <div>
@@ -16,12 +60,14 @@ const Payment = () => {
             <div className="w-[7rem]">
               <div
                 style={{ backgroundImage: `url(${paypal})` }}
+                onClick={razorPay}
                 className="bg-center  bg-[length:60px] bg-no-repeat  border-2 px-2 py-5 hover:border-[#ffdfb3] cursor-pointer  rounded-3xl "
               ></div>
             </div>
 
             <div className="w-[7rem]">
               <div
+                onClick={razorPay}
                 style={{ backgroundImage: `url(${visa})` }}
                 className="bg-center  bg-[length:35px] bg-no-repeat  border-2 px-2 py-5 hover:border-[#ffdfb3] cursor-pointer  rounded-3xl "
               ></div>
@@ -29,6 +75,7 @@ const Payment = () => {
 
             <div className="w-[7rem]">
               <div
+                onClick={razorPay}
                 style={{ backgroundImage: `url(${masterCard})` }}
                 className="bg-center  bg-[length:32px] bg-no-repeat  border-2 px-2 py-5 hover:border-[#ffdfb3] cursor-pointer  rounded-3xl "
               ></div>
@@ -37,6 +84,7 @@ const Payment = () => {
           <div className="flex w-[22rem] justify-between mt-2">
             <div className="w-[7rem]">
               <div
+                onClick={razorPay}
                 style={{ backgroundImage: `url(${maestro})` }}
                 className="bg-center  bg-[length:35px] bg-no-repeat  border-2 px-2 py-5 hover:border-[#ffdfb3] cursor-pointer  rounded-3xl "
               ></div>
@@ -44,6 +92,7 @@ const Payment = () => {
 
             <div className="w-[7rem]">
               <div
+                onClick={razorPay}
                 style={{ backgroundImage: `url(${discover})` }}
                 className="bg-center  bg-[length:60px] bg-no-repeat  border-2 px-2 py-5 hover:border-[#ffdfb3] cursor-pointer  rounded-3xl "
               ></div>
@@ -51,6 +100,7 @@ const Payment = () => {
 
             <div className="w-[7rem]">
               <div
+                onClick={razorPay}
                 style={{ backgroundImage: `url(${ideal})` }}
                 className="bg-center  bg-[length:26px] bg-no-repeat  border-2 px-2 py-5 hover:border-[#ffdfb3] cursor-pointer  rounded-3xl "
               ></div>
